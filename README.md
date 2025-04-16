ee8c7c16eb2a6ab6a0c9ef1178f64c6ad37f10c0
Adriano Ferraz <adrianoferrazguimaraes@gmail.com>
2025-04-16 20:01:49 -0300

    3 - Arquivos criados na pasta 'types'

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 
 # 2º COMMIT #
   Estruturando o projeto (Novas pastas adicionadas em src)
+
+# 3º COMMIT #
+  Criação de types (Agencia, Cliente e Conta)
---
+++ src/types/conta.ts
@@ -0,0 +1,9 @@
+export interface Conta {
+    id: string;
+    cpfCnpjCliente: string;
+    tipo: "corrente" | "poupanca";
+    saldo: number;
+    limiteCredito: number;
+    creditoDisponivel: number;
+  }
+  
